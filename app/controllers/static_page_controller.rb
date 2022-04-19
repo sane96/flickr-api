61b9add01271b043ea6d34beba7a856d620f27bb
Danilo <danilosantrac96@gmail.com>
2022-04-19 22:39:36 +0200

    Add project to repo

---
+++ app/controllers/static_page_controller.rb
@@ -0,0 +1,14 @@
+class StaticPageController < ApplicationController
+  require 'flickr'
+
+  def index
+    flickr = Flickr.new
+      unless params[:user_id].blank?
+        @photos = flickr.photos.search(user_id: params[:user_id])
+      else
+        @photos = flickr.photos.getRecent 
+      end
+  end
+
+
+end
